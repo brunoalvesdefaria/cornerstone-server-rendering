2d8ffb1546557dbda4f9d3705bb61220cdb4085c
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2018-03-07 16:33:41 -0300

    PWV-258: Attempting to render a cornerstone image on canvas

--- src/main.js
+++ src/main.js
@@ -1,19 +1,37 @@
-var canvas = require('canvas');
-var createCanvas = canvas.createCanvas;
-var loadImage = canvas.loadImage;
-var mainCanvas = createCanvas(512, 512);
-global.document = {
-  createElement: function (elementName) {
-    return mainCanvas;
-  },
-};
+var jsdom = require('jsdom');
+var JSDOM = jsdom.JSDOM;
 
+// var htmlElement = require('html-element');
+// global.window = htmlElement.window;
+// global.document = htmlElement.document;
+global.window = (new JSDOM('', { pretendToBeVisual: true })).window;
+global.document = global.window.document;
+global.CustomEvent = window.CustomEvent;
+
+global.canvas = require('canvas-prebuilt');
+// var createCanvas = canvas.createCanvas;
+// var loadImage = canvas.loadImage;
+// var mainCanvas = createCanvas(512, 512);
+// global.document = {
+//   createElement: function (elementName) {
+//     return mainCanvas;
+//   },
+// };
+
+console.warn('>>>>INIT');
 var cornerstone = require('cornerstone-core');
+var containerElement = document.createElement('div');
+// var canvas = canvasLib.createCanvas(512, 512);
+// containerElement.appendChild(canvas);
 
-var ctx = mainCanvas.getContext('2d');
+cornerstone.enable(containerElement);
+var enabledElement = cornerstone.getEnabledElement(containerElement);
+
+var ctx = enabledElement.canvas.getContext('2d');
+console.warn('>>>>canvas', enabledElement.canvas);
 ctx.strokeStyle = 'rgba(0,0,0,0.5)';
 ctx.beginPath();
 ctx.lineTo(50, 102);
 ctx.lineTo(100, 102);
 ctx.stroke();
-console.warn('<img src="' + mainCanvas.toDataURL() + '" />');
+console.warn('<img src="' + enabledElement.canvas.toDataURL() + '" />');
